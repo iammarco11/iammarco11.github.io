441db91a1fd3093f83b1bfdd7609f3239be7b1a5
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2019-12-18 07:10:35 +0530

    Basic syntax for dp to be zoomed

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,7 +3,17 @@ var modal = '';
 function open_modal(id){
     modal = document.getElementById(id)
     modal.style.display = "block"
+    console.log(modal)
 }
 function close_modal(){
     modal.style.display = "none"
+}
+
+var dp = ''
+
+function on_hover(id){
+       
+    k = document.getElementById(id) 
+    k.style.display = "block"
+    console.log(k)
 }
